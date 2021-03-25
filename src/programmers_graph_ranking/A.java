779cd3d7e8c039e9fe898265eb9d1dc5fc9314f6
wesbin <leegosoo11@naver.com>
2021-03-25 16:17:55 +0900

    programmers graph ranking

--- src/programmers_graph_ranking/A.java
+++ src/programmers_graph_ranking/A.java
@@ -8,6 +8,38 @@ public class A {
         * input
         * */
         int n = 5;
-        int[][] results = {{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}};
+        int[][] results = {{4, 3}, {2, 4}, {3, 2}, {1, 2}, {2, 5}};
+        int answer = 0;
+
+        // input 을 테이블로 전환
+        boolean[][] table = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            // true 이겻다
+            table[results[i][0] - 1][results[i][1] - 1] = true;
+        }
+        // 플로이드 워셜 알고리즘
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (j != k && table[j][i] && table[i][k]) {
+                        table[j][k] = true;
+                    }
+                }
+            }
+        }
+        //
+        for (int i = 0; i < n; i++) {
+            boolean pass = true;
+            for (int j = 0; j < n; j++) {
+                if (i != j && !(table[i][j] || table[j][i])) {
+                    pass = false;
+                    break;
+                }
+            }
+            if (pass) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
     }
 }
