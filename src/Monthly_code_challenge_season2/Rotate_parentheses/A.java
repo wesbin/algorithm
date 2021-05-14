8fc8ade0b6d03e9abdce698d27240a5b8069e536
wesbin <leegosoo11@naver.com>
2021-05-14 13:15:48 +0900

    + change directory structure + add monthly code challenge season2 Q1

---
+++ src/Monthly_code_challenge_season2/Rotate_parentheses/A.java
@@ -0,0 +1,19 @@
+package Monthly_code_challenge_season2.Rotate_parentheses;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class A {
+
+    public static void main(String[] args) {
+
+        String s = "[](){}";
+
+        for (int i = 0; i < s.length(); i++) {
+            Queue<String> q = new LinkedList<>();
+            q.add(i + "");
+
+
+        }
+    }
+}
