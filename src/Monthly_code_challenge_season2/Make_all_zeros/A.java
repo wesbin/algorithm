a9e34f3216ff988a9066680b9a4f0b91051c01d4
wesbin <leegosoo11@naver.com>
2021-05-14 17:55:47 +0900

    + Question

---
+++ src/Monthly_code_challenge_season2/Make_all_zeros/A.java
@@ -0,0 +1,18 @@
+package Monthly_code_challenge_season2.Make_all_zeros;
+
+import java.util.Arrays;
+
+public class A {
+
+    public static void main(String[] args) {
+
+        int[] a = new int[]{-5, 0, 2, 1, 2};
+
+        int[][] edges = {
+                {0, 1},
+                {3, 4},
+                {2, 3},
+                {0, 3}
+        };
+    }
+}
