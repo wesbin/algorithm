8c92cd0ff6a649284da099c195d453272c854b5b
wesbin <leegosoo11@naver.com>
2021-05-14 14:56:09 +0900

    + answer

--- src/Monthly_code_challenge_season2/Rotate_parentheses/A.java
+++ src/Monthly_code_challenge_season2/Rotate_parentheses/A.java
@@ -1,7 +1,6 @@
 package Monthly_code_challenge_season2.Rotate_parentheses;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 
 public class A {
 
@@ -9,11 +8,56 @@ public class A {
 
         String s = "[](){}";
 
+        // 사용하기 좋게 배열로
+        String[] S = s.split("");
+
+        int answer = s.length();
+        int start = 0; // 시작점
+
         for (int i = 0; i < s.length(); i++) {
-            Queue<String> q = new LinkedList<>();
-            q.add(i + "");
-
-
+            Stack<String> stack = new Stack<>();
+            for (int j = 0; j < s.length(); j++) {
+                // 시작점부터 마지막 문자열까지
+                int next = start + j;
+                if (next >= s.length()) {
+                    next -= s.length();
+                }
+//                System.out.print(S[next]);
+                // 괄호의 방향을 확인
+                if (chkRotate(S[next])) {
+                    // 여는 괄호라면 스택에 추가
+                    stack.push(S[next]);
+                } else {
+                    // 닫는 괄호라면 스택에서 꺼내 매칭
+                    if (stack.isEmpty() || !match(stack.pop(), S[next])) {
+                        // 스택이 비어있거나 맞물리지 않는다면 중단
+                        answer--;
+                        break;
+                    }
+                }
+            }
+            if (!stack.isEmpty()) {
+                answer--;
+            }
+            start++;
+//            System.out.println();
         }
+        System.out.println("answer: " + (Math.max(answer, 0)));
+    }
+    // 여는 괄호 true, 닫는 괄호 false
+    static private boolean chkRotate(String s) {
+        return switch (s) {
+            case "[", "{", "(" -> true;
+            default -> false;
+        };
+    }
+    // 두 괄호가 맞물리는지 확인
+    static private boolean match(String open, String close) {
+        return switch (open) {
+            case "[" -> close.equals("]");
+            case "{" -> close.equals("}");
+            case "(" -> close.equals(")");
+            default -> throw new IllegalStateException("Unexpected value: " + open);
+        };
     }
 }
