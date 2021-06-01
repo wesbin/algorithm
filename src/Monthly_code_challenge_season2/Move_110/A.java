7cab83358473d735ebc8f98d737901795d7a0b45
wesbin <leegosoo11@naver.com>
2021-06-01 16:04:44 +0900

    + answering... runtime error

--- src/Monthly_code_challenge_season2/Move_110/A.java
+++ src/Monthly_code_challenge_season2/Move_110/A.java
@@ -1,31 +1,46 @@
 package Monthly_code_challenge_season2.Move_110;
 
+import java.util.Stack;
+
 public class A {
 
     public String[] solution(String[] s) {
-        String[] answer = {};
-
-        /*
-        *
-        * 0 이 최대한 앞으로
-        *
-        * 마지막 0 뒤에
-        * 0이 없다면 가장 앞에 있는 1 앞에
-        *
-        * */
-
+        String[] answer = new String[s.length];
         for (int i = 0; i < s.length; i++) {
             char[] s_arr = s[i].toCharArray();
-            char[] bucket = new char[s_arr.length];
+            Stack<Character> bucket = new Stack<>();
+            int cnt = 0;
 
             for (int j = 0; j < s_arr.length; j++) {
-                bucket[j] = s_arr[j];
-
-                if (j >= 2 && bucket[j - 2] == 1 && bucket[j - 1] == 1 && bucket[j] == 0) {
-
+                if (s_arr[j] == '0') {
+                    int index = bucket.size() - 1;
+                    if (index >= 1 && bucket.get(index - 1) == '1' && bucket.get(index) == '1') {
+                        cnt++;
+                        bucket.pop();
+                        bucket.pop();
+                        continue;
+                    }
                 }
+                bucket.push(s_arr[j]);
             }
 
+            for (int j = 0; j < cnt; j++) {
+                int index = 0;
+                if (bucket.contains('0')) {
+                    index = bucket.lastIndexOf('0') + 1;
+                } else {
+                    index = bucket.indexOf('1');
+                }
+                bucket.add(index, '0');
+                bucket.add(index, '1');
+                bucket.add(index, '1');
+            }
+
+            StringBuffer sb = new StringBuffer();
+            for (Character character : bucket) {
+                sb.append(character);
+            }
+            answer[i] = sb.toString();
         }
 
         return answer;
@@ -33,8 +48,7 @@ public class A {
 
     public static void main(String[] args) {
         A A = new A();
-        String[] s = {"1110", "100111100", "0111111010"};
-
+        String[] s = {"1011110","01110","101101111010"};
         for (String s1 : A.solution(s)) {
             System.out.println(s1);
         }
